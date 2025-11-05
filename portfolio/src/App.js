899d448ebf7d7a871bc5376cf86beb9d3e9ad71b
Teja Munikrishna Geddam <teja8geddam99@gmail.com>
2025-11-05 22:38:13 +0530

    First Commit

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,18 +3,19 @@ import './App.css';
 import Footer from './components/Footer';
 import Main from './components/Main';
 import NavBar from './components/NavBar';
-import { BrowserRouter} from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
+// ... rest of your code using <
 
 function App() {
   return (
-    <BrowserRouter basename="/My_Portfolio">
+    <Router basename="/My_Portfolio">
         <div className="App">
           <h1 className='heading'>Portfolio</h1>
           <NavBar />
           <Main />
           <Footer />
         </div>
-    </BrowserRouter>
+    </Router>
   );
 }
 
